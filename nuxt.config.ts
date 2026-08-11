a730002c72abea85106487380fcf55466fed5656
Kristian <kristianmartinezgarcia@gmail.com>
2026-08-11 16:48:07 +0100

    chore: add content, testing, and lint tooling
    Add @nuxt/content, @nuxt/test-utils, @nuxt/eslint, vitest, and
    @vue/test-utils. Configure the vercel nitro preset, flat ESLint config,
    and vitest with the Nuxt test environment. Replace the NuxtWelcome
    placeholder in app.vue with NuxtPage now that routing is coming in a
    later work unit.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/content', '@nuxt/test-utils', '@nuxt/eslint'],
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    preset: 'vercel'
+  }
 })
